907b6e2fbc6b703777a4d1563bd4379fea261863
Rayhan Mahmud Shihab <whoisshihab@gmail.com>
2016-05-10 01:58:37 +0600

    Update Football_12673.cpp

--- UVa/Football_12673.cpp
+++ UVa/Football_12673.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
-#define dbug(x) cout << '>' << #x << ':' << x << endl
-#define pline    cout << "_________________________" << endl
-#define mem(x, v) memset(x, v, sizeof(x))
+#define dbug(x)     cout << '>' << #x << ':' << x << endl
+#define pline       cout << "_________________________" << endl
+#define mem(x, v)   memset(x, v, sizeof(x))
 
 
-#define eef else if
-#define sf scanf
-#define pf printf
-#define i64 long long
-#define ll  long long
-#define ui64 unsigned long long
+#define eef         else if
+#define sf          scanf
+#define pf          printf
+#define i64         long long
+#define ll          long long
+#define ui64        unsigned long long
 
 #define pcount(num)  __builtin_popcount(num)
-#define all(x) x.begin(), x.end()
-#define lla(x) x.rbegin(), x.rend()
-#define SORT(c) sort(all(c))
-#define ssort(v) stable_sort(v.begin(), v.end())
-#define sz(v) (int)(v).size()
-#define _lst(X) (X)[sz((X))-1]
+#define all(x)      x.begin(), x.end()
+#define lla(x)      x.rbegin(), x.rend()
+#define SORT(c)     sort(all(c))
+#define ssort(v)    stable_sort(v.begin(), v.end())
+#define sz(v)       (int)(v).size()
+#define _lst(X)     (X)[sz((X))-1]
 
-#define IT iterator
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define fi first
-#define se second
-#define CTN(T, x) (T.find(x) != T.end())
+#define IT          iterator
+#define pb          push_back
+#define ppb         pop_back
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define CTN(T, x)   (T.find(x) != T.end())
 
 #define max3(a,b,c) max(a,max(b,c))
 #define min3(a,b,c) min(a,min(b,c))
@@ -39,17 +39,17 @@
 #define FORIT(i, m) for (__typeof((m).begin()) i=(m).begin(); i!=(m).end(); ++i)
 
 #define siter(n,T1)  for(set<T1>::iterator it=n.begin();it!=n.end();it++)
-#define miter(n,T1,T2)  for(map<T1,T2>::iterator it=n.begin();it!=n.end();it++)
+#define miter(n,T1,T2) for(map<T1,T2>::iterator it=n.begin();it!=n.end();it++)
 
-#define ps(x) cout<<"Case "<<++x<<": "
-#define pcs(x) pf("Case %d: ", ++x)
-#define newl '\n'
-#define Newl "\n"
-#define nl puts ("")
-#define sqr(a) ((a)*(a))
-#define MAX 1e12
+#define ps(x)       cout<<"Case "<<++x<<": "
+#define pcs(x)      pf("Case %d: ", ++x)
+#define newl        '\n'
+#define Newl        "\n"
+#define nl          puts("")
+#define sqr(a)      ((a)*(a))
+#define MAX         1e12
 
-#define FAST ios_base::sync_with_stdio(0)
+#define FAST        ios_base::sync_with_stdio(0)
 using namespace std;
 
 
