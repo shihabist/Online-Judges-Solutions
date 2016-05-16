9d4365a11916872dc5e298a84bb8feb889e48f88
Rayhan Mahmud Shihab <whoisshihab@gmail.com>
2016-05-16 17:58:06 +0600

    Create 1293_Eniya.cpp

---
+++ Timus/1293_Eniya.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define newl '\n'
+#define FAST ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FAST;
+    int a, b, c;
+    while(cin >> a >> b >> c){
+        cout << a*b*c*2 << newl;
+    }
+    return 0;
+}
